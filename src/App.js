1ed02f3827ffe88207023eb1c4035ffb368e0650
Annie Chiang <devbear22@gmail.com>
2018-01-07 14:32:34 -0800

    add intro sections & forms to page

--- src/App.js
+++ src/App.js
@@ -2,17 +2,23 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './styles/App.css';
 
+import IntroSection from './landing-page/intro/intro-section.component';
+import LoginForm from './landing-page/login-signup/login-form.component';
+import SignUpFrom from './landing-page/login-signup/signup-form.component';
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           {/* <img src={logo} className="App-logo" alt="logo" /> */}
-          <h1 className="App-title">GuestBook</h1>
+          <h1 className="App-title">Guest Book</h1>
         </header>
         <p className="App-intro">
-          Manage your guests with GuestBook!
+          Welcome to Guest Book! This app will change how you plan future events!
         </p>
+        <IntroSection />
+        <LoginForm />
+        <SignUpFrom />
       </div>
     );
   }
