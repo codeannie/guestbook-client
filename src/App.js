d057d27a5b8387bd7b07cd3a23732851266b6b24
Annie Chiang <devbear22@gmail.com>
2018-01-07 15:09:54 -0800

    remove extra unused bootstrap items

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
 import './styles/App.css';
 
 import IntroSection from './landing-page/intro/intro-section.component';
 import LoginForm from './landing-page/login-signup/login-form.component';
 import SignUpFrom from './landing-page/login-signup/signup-form.component';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="header-image">
           <header className="App-header">
-            {/* <img src={logo} className="App-logo" alt="logo" /> */}
             <h1 className="App-title">Guest Book</h1>
           </header>
         </div>
