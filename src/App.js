8a292038488c2f5d4e62b33c127cbc3be797375c
Annie Chiang <devbear22@gmail.com>
2018-01-07 15:08:27 -0800

    add header background image test

--- src/App.js
+++ src/App.js
@@ -9,16 +9,22 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          {/* <img src={logo} className="App-logo" alt="logo" /> */}
-          <h1 className="App-title">Guest Book</h1>
-        </header>
-        <p className="App-intro">
-          Welcome to Guest Book! This app will change how you plan future events!
-        </p>
-        <IntroSection />
-        <LoginForm />
-        <SignUpFrom />
+        <div className="header-image">
+          <header className="App-header">
+            {/* <img src={logo} className="App-logo" alt="logo" /> */}
+            <h1 className="App-title">Guest Book</h1>
+          </header>
+        </div>
+        {/* <div className="intro-container"> */}
+          <p className="App-intro">
+            Welcome to Guest Book! This app will change how you plan future events!
+          </p>
+          <IntroSection />
+        {/* </div> */}
+        <div className="form-container">
+          <LoginForm />
+          <SignUpFrom />
+        </div>
       </div>
     );
   }
