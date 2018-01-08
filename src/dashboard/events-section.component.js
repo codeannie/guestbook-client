784f1420ef3157cb77d0fab2510797af04d615da
Annie Chiang <devbear22@gmail.com>
2018-01-07 20:18:33 -0800

    add mapping for event cards and update to semantic html

--- src/dashboard/events-section.component.js
+++ src/dashboard/events-section.component.js
@@ -9,14 +9,18 @@ export default class EventSection extends React.Component {
   //   super(props);
   // }
 
-  render () {
-    // const eventCards  -- mapping? 
-    // props.events.map = transform event to eventCard component
+  render() {
+    const eventCards = this.props.events.map((event, index) => 
+      <div className="event" key={index}>
+        <Event Card {...event} />
+      </div>
+    );
+
     return (
-      <div className="events-section">
-        <h3> {props.title} </h3>
-        {/* put the constant of the mapped array here `*/}
-      </div> 
+      <section className="events-section">
+        <h3> {this.props.title} </h3>
+          {eventCards}
+      </section> 
     )
   }
 }
