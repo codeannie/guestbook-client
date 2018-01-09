7420fcf7fa01a90149dfc791b6af7cff27c0ca19
Annie Chiang <devbear22@gmail.com>
2018-01-08 23:01:34 -0800

    remove Provider/Store to index & resolve MuiThemeError (only 1 child)

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import React, { Component } from 'react';
 import './styles/App.css';
-import { Provider } from 'react-redux'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
-import store from './store';
 import LandingPage from './landing-page/landing-page.component';
+import Dashboard from './dashboard/dashboard.component'
 class App extends Component {
 
   //not sure if App div is needed
   render() {
     return (
-      <Provider store={store}>
-        <MuiThemeProvider>
+      <MuiThemeProvider>
+        <div>
           <LandingPage />
-        </MuiThemeProvider>
-      </Provider>
+          <Dashboard />
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
