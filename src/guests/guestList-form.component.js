5f5a20a309363292740bbcfa93c12470f46251b5
Annie Chiang <devbear22@gmail.com>
2018-01-09 14:40:15 -0800

    initial guest form

---
+++ src/guests/guestList-form.component.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Field, reduxForm } from 'redux-form';
+import { TextField } from 'redux-form-material-ui'
+import SubmitBtn from '../_shared/buttons/submit-btn.component'
+import ResetBtn from '../_shared/buttons/reset-btn.component'
+import CancelBtn from '../_shared/buttons/cancel-btn.component'
+import SendBtn from '../_shared/buttons/send-btn.component'
+
+class GuestForm extends React.Component{
+  render() {
+    const { handleSubmit } = this.props;
+  
+    return (
+      <div className="form-container">
+        <form onSubmit={handleSubmit}>
+          <Field label="First Name"
+            name="firstName"
+            component={TextField}
+            floatingLabelText="First Name"/>
+          
+          <Field label="Last Name"
+            name="lastName"
+            component={TextField}
+            floatingLabelText="Last Name" />
+
+          <Field label="E-mail"
+            name="email"
+            component={TextField}
+            floatingLabelText="E-mail" />
+
+          <SubmitBtn buttonName="Save" />
+          <SendBtn buttonName="Send" />
+          <CancelBtn buttonName="Close" />
+        </form>
+      </div>
+    )
+  }
+}
+
+Guestorm = reduxForm({
+  form: 'guests'
+})(GuestForm);
+
+export default GuestForm;
