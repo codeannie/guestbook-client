79fa52fd82f7c126ff429bfb5571efb482e46fc1
Annie Chiang <devbear22@gmail.com>
2018-01-09 14:39:25 -0800

    change component name to be more generic and will be reused in dashboard

--- src/dashboard/dashboard.component.js
+++ src/dashboard/dashboard.component.js
@@ -1,6 +1,7 @@
 // this will hold teh dashboard content...? 
 // or does this go into App.js and use routing? 
 import React from 'react';
+import AppBar from '../_shared/navigation/app-nav.component';
 import Welcome from './welcome.component';
 import EventSection from './events-section.component';
 import EventForm from '../events/event-form.component';
@@ -8,6 +9,7 @@ export default class Dashboard extends React.Component {
   render() {
     return (
       <div className="dashboard-container">
+        {/* <AppBar /> */}
         <Welcome />
         <EventSection />
         <EventForm />
