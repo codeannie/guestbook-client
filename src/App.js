ea2fbf8317a9975871b3befa96830ed518b30a44
Annie Chiang <devbear22@gmail.com>
2018-01-08 21:16:18 -0800

    add redux store & material ui

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
 import './styles/App.css';
+import { Provider } from 'react-redux'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
+import store from './store';
 import LandingPage from './landing-page/landing-page.component';
 class App extends Component {
+
+  //not sure if App div is needed
   render() {
     return (
-      <div className="App">
-      <LandingPage />
-      </div>
+      <Provider store={store}>
+        <MuiThemeProvider>
+          <LandingPage />
+        </MuiThemeProvider>
+      </Provider>
     );
   }
 }
