a2c686358d682aca51fbb19a225805dcb4cf3a4b
Annie Chiang <devbear22@gmail.com>
2018-01-10 16:40:09 -0800

    initial attempt to get material-iu appbar and drawer

---
+++ src/_shared/navigation/app-nav.component.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Appbar from 'material-ui/AppBar';
+
+const AppBar = (props) => {
+  return (
+    <AppBar
+      title="Guest Book"
+    />
+  )
+}
+
+export default AppBar;
