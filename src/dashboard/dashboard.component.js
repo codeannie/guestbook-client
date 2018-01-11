e0589b320c5a6dd7b5d37f007da477886f3d5d04
Annie Chiang <devbear22@gmail.com>
2018-01-11 15:18:46 -0800

    add in forms to view for editing

--- src/dashboard/dashboard.component.js
+++ src/dashboard/dashboard.component.js
@@ -2,6 +2,7 @@
 // or does this go into App.js and use routing? 
 import React from 'react';
 import AppNav from '../_shared/navigation/app-nav.component';
+import NavBarDrawer from '../_shared/navigation/nav-bar-drawer.component';
 import Welcome from './welcome.component';
 import EventSection from './events-section.component';
 import EventForm from '../events/event-form.component';
@@ -11,12 +12,12 @@ export default class Dashboard extends React.Component {
   render() {
     return (
       <div className="dashboard-container">
-        <AppNav />
+        <NavBarDrawer />
         <Welcome />
         <EventSection />
-        {/* <EventForm />
+        <EventForm />
         <EventOverview />
-        <GuestForm /> */}
+        <GuestForm />
       </div>
     )
   }
