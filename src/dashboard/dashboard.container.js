21c337fb73096308354e8208d14ba50b51b14fb0
Annie Chiang <devbear22@gmail.com>
2018-01-20 13:19:37 -0800

    consolidate to dashboard-only items; separated nav bar items

--- src/dashboard/dashboard.container.js
+++ src/dashboard/dashboard.container.js
@@ -1,30 +1,23 @@
-import { connect } from 'react-redux';
-import { push } from 'redux-little-router';
-import Dashboard from './dashboard.component';
+import { connect } from "react-redux";
+import { push } from "redux-little-router";
+import Dashboard from "./dashboard.component";
 // import actions
-import { createGetAllEventsAction } from '../events/store/events.actions';
-import { createLogOutAction } from '../_shared/store/session/session.actions';
+import { createGetAllEventsAction } from "../events/store/events.actions";
 
 const mapStateToProps = state => {
   return {
-    // at login, should give you current user 
+    // at login, should give you current user
     currentUser: state._sharedReducer.session.currentUser,
     events: state.eventsReducer.events
   };
 };
 
 const mapDispatchToProps = dispatch => {
-  // const userId = state._sharedReducer.session.currentUser.user.id; //?
   return {
-  //   onLogOut() {
-  //     if (!state._sharedReducer.session.currentUserUser) {
-  //       dispatch(createLogOutAction());
-  //       dispatch(push('/'));
-  //     }
-      onLoadDashboard() {
-        dispatch(createGetAllEventsAction());
-      }
+    onLoadDashboard: () => {
+      dispatch(createGetAllEventsAction());
+    }
   };
-}
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(Dashboard);
