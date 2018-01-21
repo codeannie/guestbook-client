b5fd96c20ae6a8cd1d34a545f25fed7f5f8494a5
Annie Chiang <devbear22@gmail.com>
2018-01-21 13:36:18 -0800

    initial styling for landing page

--- src/landing-page/landing-page.component.js
+++ src/landing-page/landing-page.component.js
@@ -4,16 +4,14 @@ import Header from '../_shared/navigation/header-image.component';
 import IntroSection from './intro/intro-section.component';
 import LoginForm from './login-signup/login-form.container';
 import SignUpForm from './login-signup/signup-form.container';
+import styles from './landing-page.styles';
 
 export default class LandingPage extends React.Component {
   render() {
     return (
       <div className="main-container">
         <Header />
-      <div className="landing-container">
-        <p className="App-intro">
-          Welcome to Guest Book! This app will change how you plan future events!
-        </p>
+      <div className="landing-container" style={styles.container}>
         <IntroSection />
         <LoginForm />
         <SignUpForm />
---
+++ src/landing-page/landing-page.styles.js
@@ -0,0 +1,24 @@
+import bulletStyles from '../_shared/styles/bullets.style.component';
+
+const styles = {
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    padding: '1em'
+
+  },
+  header: {
+    textAlign: 'center'
+  },
+  paragraph: {
+    textAlign: 'center'
+  },
+  list: {
+    listStylePosition: 'inside',
+    // textAlign: 'center',
+    marginRight: 'auto',
+  }
+}
+
+export default styles; 
