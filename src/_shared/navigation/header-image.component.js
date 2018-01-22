e1ab242effc001635754144f1513394150d0b410
Annie Chiang <devbear22@gmail.com>
2018-01-21 18:00:09 -0800

    separate buttons from header image

--- src/_shared/navigation/header-image.component.js
+++ src/_shared/navigation/header-image.component.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import { FlatButton } from 'material-ui';
 import styles from './header-image.styles';
 
 const headerImage = (props) => {
   return (
-    // <div className="header-bar">
-      <header className="App-header" style={styles.headerBackground}>
-        <h1 className="App-title" style={styles.title}> - GUEST BOOK - </h1>
-      </header>
-    // </div>
+    <header className="App-header" style={styles.headerBackground}>
+      <h1 className="App-title" style={styles.title}> - GUEST BOOK - </h1>
+    </header>
   )
 }
 
