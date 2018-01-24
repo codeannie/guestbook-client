948d59b72840eb690bb3cc4b3697e23b5768d2cb
Annie Chiang <devbear22@gmail.com>
2018-01-23 18:02:45 -0800

    change landing page to aphrodite

--- src/landing-page/intro/intro-section.component.js
+++ src/landing-page/intro/intro-section.component.js
@@ -1,20 +1,22 @@
 import React from 'react';
+import { css } from 'aphrodite';
 import IntroCard from './intro-card.component';
 import styles from '../landing-page.styles';
+import sharedStyles from '../../_shared/styles/shared.styles';
 
 const IntroSection = (props) => {
   return (
     <section>
-      <h1 style={styles.header1}> Welcome to Guest Book! </h1>
-        <p style={styles.paragraph}> Streamline how you create events and manage the guest list </p>
-        <h2 style={styles.header2}> - WHAT - </h2>
-          <p style={styles.paragraph}> Manage your guests in one place </p>
-          <p style={styles.paragraph}>Track RSVP status and attendence </p>
+      <h1 className={css(sharedStyles.headerFont, styles.header1)}> Welcome to Guest Book! </h1>
+        <p className={css(sharedStyles.bodyFont, styles.centerText)}> Streamline how you create events and manage the guest list </p>
+        <h2 className={css(sharedStyles.headerFont, styles.centerText)}> - WHAT - </h2>
+          <p className={css(sharedStyles.bodyFont, styles.centerText)}> Manage your guests in one place </p>
+          <p className={css(sharedStyles.bodyFont, styles.centerText)}>Track RSVP status and attendence </p>
 
-        <h2 style={styles.header2}> - HOW - </h2>
-          <p style={styles.paragraph}> Create the Event </p>
-          <p style={styles.paragraph}> Create the Guest List </p>
-          <p style={styles.paragraph}> Check-in Guests on Day of Event </p>
+        <h2 className={css(sharedStyles.headerFont, styles.centerText)}> - HOW - </h2>
+          <p className={css(sharedStyles.bodyFont, styles.centerText)}> Create the Event </p>
+          <p className={css(sharedStyles.bodyFont, styles.centerText)}> Create the Guest List </p>
+          <p className={css(sharedStyles.bodyFont, styles.centerText)}> Check-in Guests on Day of Event </p>
     </section>  
   );
 }
--- src/landing-page/landing-page.component.js
+++ src/landing-page/landing-page.component.js
@@ -1,36 +1,37 @@
 // put landing page components here
 import React from 'react';
+import { css } from 'aphrodite';
 import { FlatButton } from 'material-ui';
-
 import Header from '../_shared/navigation/header-image.component';
 import IntroSection from './intro/intro-section.component';
 import LoginForm from './login-signup/login-form.container';
 import SignUpForm from './login-signup/signup-form.container';
 import styles from './landing-page.styles';
+import sharedStyles from '../_shared/styles/shared.styles';
 
 export default class LandingPage extends React.Component {
   render() {
     return (
       <div className="main-container">
         <Header />
-        <div className="btn-container" style={styles.flex}>
+        <div className={css(styles.flexContainer)} style={styles.flex}>
 
         <FlatButton 
             label="Demo"
             onClick={() => {this.props.onLoginDemo()}}
-            style={styles.button} />
+            style={btnStyle} />
 
           <FlatButton 
             label="Sign Up"
             onClick={() => {this.props.openSignUpForm()}}
-            style={styles.button} />
+            style={btnStyle} />
 
           <FlatButton 
             label="Log in"
             onClick={() => {this.props.openLoginForm()}}
-            style={styles.button} />
+            style={btnStyle} />
         </div>
-      <div className="landing-container" style={styles.container}>
+      <div className={css(sharedStyles.mainContainer)}>
         <IntroSection />
         <LoginForm />
         <SignUpForm />
@@ -38,4 +39,9 @@ export default class LandingPage extends React.Component {
     </div>
     )
   }
+}
+
+const btnStyle = {
+  color: 'white',
+  fontSize: 14,
 }
