312ef0ce8046f78860b0ebfd21f0a468035ae266
Annie Chiang <devbear22@gmail.com>
2018-01-23 21:42:18 -0800

    add login & signup route for landing page butn navigation

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { Fragment as RouterFragment } from "redux-little-router";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import muiTheme from "./_shared/styles/material-ui.theme";
 import LandingPage from "./landing-page/landing-page.container";
+import LoginForm from './landing-page/login-signup/login-form.container';
+import SignUpForm from './landing-page/login-signup/signup-form.container';
 import { Dashboard } from "./dashboard";
 import EventPage from '../src/events/event-page.container';
 class App extends Component {
@@ -16,7 +18,10 @@ class App extends Component {
               <LandingPage />
             </RouterFragment>
             <RouterFragment forRoute="/login">
-              <div>Login</div>
+              <LoginForm />
+            </RouterFragment>
+            <RouterFragment forRoute="/signup">
+              <SignUpForm />
             </RouterFragment>
             <RouterFragment forRoute="/dashboard">
               <Dashboard />
