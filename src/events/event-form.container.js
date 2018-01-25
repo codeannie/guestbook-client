3678f1f1722ddb34083fd3aa83a9e764382bb352
Annie Chiang <devbear22@gmail.com>
2018-01-25 09:11:02 -0800

    fix guest form route push after new event submit

--- src/events/event-form.container.js
+++ src/events/event-form.container.js
@@ -4,7 +4,7 @@ import { push } from '../_shared/store/router/utils';
 import EventForm from './event-form.component';
 import { createNewEventAction } from './store/events.actions';
 import { createEvent, modifyEvent } from './services/events.service';
-
+import { GUESTLIST_NEW_ROUTE } from '../_shared/store/router/authenticated.routes';
 const mapStateToProps = state => {
   // store user info in props
   let currentUser; 
@@ -23,7 +23,9 @@ const mapDispatchToProps = dispatch => {
       createEvent(newEvent)
       .then(res => {
         dispatch(createNewEventAction(res.data));
-        dispatch(push('/guestlist'));
+        dispatch(push({
+          route: GUESTLIST_NEW_ROUTE
+        }));
       })
     }
   };
