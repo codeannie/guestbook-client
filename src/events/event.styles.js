0162a0bb949ad7ee6aa86747908cab53118249e2
Annie Chiang <devbear22@gmail.com>
2018-01-25 17:23:00 -0800

    rename file & have one style file for events

---
+++ src/events/event.styles.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'aphrodite';
+
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: '0 auto'
+  },
+});
+
+export default styles; 
