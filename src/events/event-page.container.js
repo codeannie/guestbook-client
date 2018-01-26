f986a4bd08de5e66a7e813816a2031994116dbc7
Annie Chiang <devbear22@gmail.com>
2018-01-26 11:32:37 -0800

    remove number() from container & add eventId in StateToProps; commented out function for edit form button

--- src/events/event-page.container.js
+++ src/events/event-page.container.js
@@ -1,20 +1,23 @@
 import { connect } from "react-redux";
+import { push } from '../_shared/store/router/utils';
 import EventPage from "./event-page.component";
-// import action
+import { EVENT_EDIT_ROUTE } from '../_shared/store/router/authenticated.routes';
 
 const mapStateToProps = state => {
   // get all events from reducer
   const events = state.eventsReducer.events;
   // get param from router - event card container 
   const paramId = state.router.params.eventId;
+  console.log('event page container - paramId ->', paramId);
   // find specific event
   const event = events.find(e => {
-    return e.id === paramId 
+    return e.id === paramId;
   })
-  
+  console.log('event page container event->', event);
   return {
     // return single event
     event,
+    eventId: state.router.params.eventId,
     route: state.router.route
   };
 };
@@ -23,6 +26,14 @@ const mapDispatchToProps = dispatch => {
   return {
     // getEvents(userId) {
     //   dispatch(createGetAllEventsAction(userId));
+    // }
+    // onEditClick(eventId) {
+    //   dispatch(push({
+    //     route: EVENT_EDIT_ROUTE,
+    //     params: {
+    //       eventId
+    //     }
+    //   }));
     // }
   };
 };
