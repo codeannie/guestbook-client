8e7e520f7abcdba477ccc7e79b52278e65e29982
Annie Chiang <devbear22@gmail.com>
2018-01-31 20:01:55 -0800

    remove mock event data

--- src/events/store/events.reducer.js
+++ src/events/store/events.reducer.js
@@ -7,49 +7,7 @@ import {
 } from './events.actions';
 
 const initialState = {
-  // eventually change this to events: [] 
-  events: [
-    {
-      id: '1',
-      eventName: 'Test Event',
-      description: 'blah',
-      date: '2/1/18',
-      startTime: '1PM',
-      endTime: '4PM',
-      locationName: 'Somewhere over the rainbow',
-      locationAddress: 'this is an address',
-      locationLink: 'this is a lcoation link',
-      numOfGuests: 10,
-      eventStatus: 1
-    }, {
-      id: '2',
-      eventName: 'Test Event 2',
-      date: '2/4/18',
-      startTime: '1PM',
-      endTime: '4PM',
-      locationName: 'Somewhere with hipster backyard',
-      numOfGuests: 40,
-      eventStatus: 1
-    }, {
-      id: '3',
-      eventName: 'Test Past Event',
-      date: '2/4/18',
-      startTime: '1PM',
-      endTime: '2PM',
-      locationName: 'hilton conference room',
-      numOfGuests: 40,
-      eventStatus: 2
-    }, {
-      id: '4',
-      eventName: 'Test Archive Event',
-      date: '2/4/18',
-      startTime: '1PM',
-      endTime: '2PM',
-      locationName: 'somewhere in the archives',
-      numOfGuests: 50,
-      eventStatus: 3
-    }
-  ]
+  events: [ ]
 };
 
 export const eventsReducer = (state = initialState, action) => {
