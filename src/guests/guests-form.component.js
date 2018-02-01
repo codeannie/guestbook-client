e7977807757223e85ceadde54615673e09e326d3
Annie Chiang <devbear22@gmail.com>
2018-01-31 20:04:43 -0800

    fix guest form style

--- src/guests/guests-form.component.js
+++ src/guests/guests-form.component.js
@@ -62,7 +62,7 @@ export default class GuestForm extends React.Component{
     return (
       <div className="guestFormContainer">
         <h2 className={css(sharedStyles.headerFont)}> Guest List for {event.eventName} </h2>
-        <div className={css(formStyles.eventGuestContainer)}>
+        <div className={css(formStyles.guestListContainer)}>
           <ul>
             {guestListItems}
           </ul>
