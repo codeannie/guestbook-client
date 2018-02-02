7e534e5c1a287c7db096c44a357a85b527ea08dd
Annie Chiang <devbear22@gmail.com>
2018-02-02 14:41:05 -0800

    remove guest rsvp status for guest list in event overview

--- src/guests/guestlist-view.component.js
+++ src/guests/guestlist-view.component.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { css } from 'aphrodite'; 
-import GuestResponse from './guest.component';
 import styles from '../events/event.styles';
 
 export default class GuestList extends React.Component {
@@ -11,40 +10,57 @@ export default class GuestList extends React.Component {
   }
   
   render() {
-    const guestResponses = new Map();
-      // no response
-      guestResponses.set(0, []);
-      // attending
-      guestResponses.set(1, []);
-      // not attending
-      guestResponses.set(2, []);
-      // tentative
-      guestResponses.set(3, []);
-
-    const guestByStatus = this.props.guests.forEach((guest, index) => {
-      //map, get array that corresponds to rsvp status & push to array
-      guestResponses.get(guest.rsvpStatus).push(
-        <li className="guestInfo" key={index}> 
-          {guest.fullName}
-        </li>
-      )
+    const { guests } = this.props;
+    
+    const guestListItems = guests.map((guest, index) => {
+      return <li key={index}>
+        <strong>{guest.fullName}</strong> - <span>{guest.email}</span>
+      </li>;
     })
 
     return (
       <section className={css(styles.guestList)} eventId={this.props.event.id}>
-        <h2> Guest List </h2>
-        {/* <h3> Responses </h3> */}
-          <h4> Attending </h4>
-            <GuestResponse guestByStatus={guestResponses.get(1)} />
-          <h4> Not Attending </h4>
-            <GuestResponse guestByStatus={guestResponses.get(2)} />
-          <h4> Tentative </h4>
-            <GuestResponse guestByStatus={guestResponses.get(3)} />
-          <h4> Not Responded </h4>
-            <GuestResponse guestByStatus={guestResponses.get(0)} />
-
+        {/* <h3> Guest List </h3> */}
+          <ol>
+            {guestListItems}
+          </ol>
       </section>
     )
   }
 }
+
+// render() {
+// const guestResponses = new Map();
+//       // no response
+//       guestResponses.set(0, []);
+//       // attending
+//       guestResponses.set(1, []);
+//       // not attending
+//       guestResponses.set(2, []);
+//       // tentative
+//       guestResponses.set(3, []);
+
+//     const guestByStatus = this.props.guests.forEach((guest, index) => {
+//       //map, get array that corresponds to rsvp status & push to array
+//       guestResponses.get(guest.rsvpStatus).push(
+//         <li className="guestInfo" key={index}> 
+//           {guest.fullName}
+//         </li>
+//       )
+//     })
+
+//     return (
+//       <section className={css(styles.guestList)} eventId={this.props.event.id}>
+//         <h2> Guest List </h2>
+//         {/* <h3> Responses </h3> */}
+//           <h4> Attending </h4>
+//             <GuestResponse guestByStatus={guestResponses.get(1)} />
+//           <h4> Not Attending </h4>
+//             <GuestResponse guestByStatus={guestResponses.get(2)} />
+//           <h4> Tentative </h4>
+//             <GuestResponse guestByStatus={guestResponses.get(3)} />
+//           <h4> Not Responded </h4>
+//             <GuestResponse guestByStatus={guestResponses.get(0)} />
+
+//       </section>
 
