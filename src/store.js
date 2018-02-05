7a1a1122ed9be9943079eeee468c936328782939
Annie Chiang <devbear22@gmail.com>
2018-02-05 08:19:21 -0800

    revise per redux dev tool docs to setup w middleware & enhancers

--- src/store.js
+++ src/store.js
@@ -3,14 +3,16 @@ import thunk from 'redux-thunk';
 import { reducer } from './reducer';
 import { routerEnhancer, routerMiddleware } from './_shared';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const store = createStore(
   reducer,
   compose(
     routerEnhancer,
-    applyMiddleware(routerMiddleware, thunk),
-    window.navigator.userAgent.includes('Chrome') ?
-      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() : compose
+    composeEnhancers(
+      applyMiddleware(routerMiddleware, thunk)
+    )
   ),
 );
-
+  
 export default store;
